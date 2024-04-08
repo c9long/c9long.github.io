1bf3aa556073a8127572a4a149d9b6fdc6b36dd1
c9long <109810526+c9long@users.noreply.github.com>
2024-04-08 12:54:53 -0400

    Initial commit

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React, { useTransition, useState } from 'react'
+import Image from 'next/image'
+import TabButton from './TabButton';
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>NodeJS</li>
+                <li>NextJS</li>
+                <li>ReactJS</li>
+                <li>PyTorch</li>
+                <li>AWS Lambda</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>University of Waterloo</li>
+                <li>Korea Advanced Institute of Science and Technology</li>
+            </ul>
+        )
+    },
+    {
+        title: "Experience",
+        id: "experience",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Manulife Cloud Platforms</li>
+                <li>Manulife Global Cyber Security</li>
+            </ul>
+        )
+    }
+]
+
+
+const AboutSection = () => {
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
+
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+
+    return (
+        <section className="text-white"><div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+            <Image src="/images/AboutMe.jpg" width={500} height={500} />
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+                <h2 className='text-4xl font-bold text-white mb-4'>
+                    About Me
+                </h2>
+                <p className='text-base lg:text-lg'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div className='flex flex-row mt-8'>
+                    <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
+                        Skills
+                    </TabButton>
+                    <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
+                        Education
+                    </TabButton>
+                    <TabButton selectTab={() => handleTabChange("experience")} active={tab === "experience"}>
+                        Experience
+                    </TabButton>
+                </div>
+                <div className='mt-8'>
+                    {TAB_DATA.find((t) => t.id === tab).content}
+                </div>
+            </div>
+        </div>
+        </section>
+    )
+}
+
+export default AboutSection
---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,63 @@
+"use client";
+import React from 'react'
+import Image from 'next/image'
+
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 text-left'>
+                <section>
+                    <h1 className="text-green-50 mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-green-50'>
+                            Welcome!
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'I\'m Christopher R. Long',
+                                1300, // wait 1s before replacing
+                                'Cloud Platforms Engineer',
+                                1000,
+                                'Site Reliability Engineer',
+                                1000,
+                                'Video Game Enthusiast',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={65}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+                        Lorem ipsum dolor sit amet
+                    </p>
+                    <div>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-green-600 hover:bg-green-700 text-white'>
+                            Download CV
+                        </button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-green-600 to-green-200 hover:bg-slate-800 text-white mt-3'>
+                            <span className='block bg-[#1e2124] hover:bg-slate-800 rounded-full px-5 py-2 '>Contact Info</span>
+                        </button>
+                    </div>
+                </section>
+            </div>
+            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                <div className='w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                    <Image
+                        src="/images/Portrait.jpg"
+                        alt="portfolio portrait"
+                        width={300}
+                        height={300}
+                        style={{ borderRadius: '50%' }}
+                        className='absolute transform -translate -x-1/2 -translate-y-1/2 top-1/2 bottom-1/2 px-5 py-5'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection

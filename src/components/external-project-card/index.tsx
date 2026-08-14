c0294c5c4ebf2d7fe8cf294648c3b56ed5712042
c9long <c9long@uwaterloo.ca>
2026-08-13 20:54:30 -0400

    Clio projects no longer misleadingly clickable

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -68,27 +68,8 @@ const ExternalProjectCard = ({
   };
 
   const renderExternalProjects = () => {
-    return externalProjects.map((item, index) => (
-      <a
-        className="card shadow-md card-sm bg-base-100 cursor-pointer"
-        key={index}
-        href={item.link}
-        onClick={(e) => {
-          e.preventDefault();
-
-          try {
-            if (googleAnalyticId) {
-              ga.event('Click External Project', {
-                post: item.title,
-              });
-            }
-          } catch (error) {
-            console.error(error);
-          }
-
-          window?.open(item.link, '_blank');
-        }}
-      >
+    return externalProjects.map((item, index) => {
+      const cardBody = (
         <div className="p-8 h-full w-full">
           <div className="flex items-center flex-col">
             <div className="w-full">
@@ -120,8 +101,41 @@ const ExternalProjectCard = ({
             </div>
           </div>
         </div>
-      </a>
-    ));
+      );
+
+      if (!item.link) {
+        return (
+          <div className="card shadow-md card-sm bg-base-100" key={index}>
+            {cardBody}
+          </div>
+        );
+      }
+
+      return (
+        <a
+          className="card shadow-md card-sm bg-base-100 cursor-pointer"
+          key={index}
+          href={item.link}
+          onClick={(e) => {
+            e.preventDefault();
+
+            try {
+              if (googleAnalyticId) {
+                ga.event('Click External Project', {
+                  post: item.title,
+                });
+              }
+            } catch (error) {
+              console.error(error);
+            }
+
+            window?.open(item.link, '_blank');
+          }}
+        >
+          {cardBody}
+        </a>
+      );
+    });
   };
 
   return (
